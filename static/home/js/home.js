2a317b4e5f879ac116dbca257176951f30eaa41d
skyber2017 <nguyenhoangduy1997@gmail.com>
2017-11-15 02:22:39 +0700

    update

--- static/home/js/home.js
+++ static/home/js/home.js
@@ -2,4 +2,24 @@
 $(window).on("load resize ", function() {
   var scrollWidth = $('.tbl-content').width() - $('.tbl-content table').width();
   $('.tbl-header').css({'padding-right':scrollWidth});
+
+ 
+  $('#btnDone').click(function() {
+  	swal({
+	  title: 'Bạn đã hoàn thành công việc!',
+	  text: "",
+	  type: 'success',
+	  showCancelButton: true,
+	  confirmButtonColor: '#3085d6',
+	  cancelButtonColor: '#d33',
+	  confirmButtonText: 'HOÀN THÀNH',
+	  cancelButtonText:'Hủy bỏ'
+	}).then(function () {
+	  swal(
+	    'RẨT TỐT!',
+	    'Xin chúc mừng bạn đã hoàn thành công việc!',
+	    'success'
+	  )
+	})
+  });
 }).resize();
