2cf250c7754e86eaa8c4014072abf04a86859e11
skyber2017 <nguyenhoangduy1997@gmail.com>
2017-11-17 15:46:15 +0700

    delete

--- app.py
+++ app.py
@@ -12,42 +12,68 @@ app.config['MYSQL_DATABASE_HOST'] = '115.84.183.142'
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 mysql.init_app(app)
 
+@app.route('/delete',methods=["POST"])
+def delete():
+	try:
+		if request.method == "POST":
+			id = request.form['id']
+			user = request.form['user']
+			if user == session.get('user') or session.get("is_admin") == 1:
+				conn = mysql.connect()
+				cursor = conn.cursor()
+				cursor.execute("SELECT * FROM job WHERE id='"+id+"'")
+				data = cursor.fetchone()
+				if data is None:
+					return jsonify(status=1,message="Cái bạn định xóa không tồn tại hoặc đã bị xóa!")
+				else:
+					cursor.execute("DELETE FROM job WHERE id='"+id+"'")
+					conn.commit()
+					return jsonify(status=0,message="Đã xóa thành công!")
+			else:
+				return jsonify(status=1,message="Bạn không đủ quyền truy cập!")
+
+	except:
+		return jsonify(status=1,message="Lỗi không xác định!")
 @app.route('/info',methods=["POST"])
 def info():
-	if request.method=="POST":
-		if session.get('user') and session.get('user') == str(request.form['user']) or session.get('user') == 'admin':
-			with mysql.connect().cursor() as cursor:
-				cursor.execute("SELECT content FROM job WHERE id='"+str(request.form['id'])+"'")
-				data = cursor.fetchone()
-				return jsonify(status=0,content=data[0])
-		else:
-			return jsonify(status=1,message="Bạn không đủ quyền để xem!")
-
+	try:
+		if request.method=="POST":
+			if session.get('user') and session.get('user') == str(request.form['user']) or session.get('is_admin') == 1:
+				with mysql.connect().cursor() as cursor:
+					cursor.execute("SELECT content FROM job WHERE id='"+str(request.form['id'])+"'")
+					data = cursor.fetchone()
+					return jsonify(status=0,content=data[0])
+			else:
+				return jsonify(status=1,message="Bạn không đủ quyền để xem!")
+	except:
+		return jsonify(status=1,message="Lỗi không xác định!")
 @app.route('/add',methods=["POST"])
 def add():
-	if request.method == "POST":
-		data = request.json
-		user = data['user']
-		title= data['title']
-		deadline = data['deadline']
-		content = data['content']
-		init = str(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
-		#a = json.dumps({title:content})
-		if title:
-			conn = mysql.connect()
-			cursor = conn.cursor()
-			cursor.execute("SELECT title FROM job WHERE title='"+title+"'")
-			data = cursor.fetchone()
-			if data is None:
-				sql = "INSERT INTO job VALUES('','"+user+"','"+init+"','"+deadline+"','"+init+"','"+title+"','"+content+"')"
-				cursor.execute(sql)
-				conn.commit()
-				return jsonify(status=0,message="Chúc mừng! Đã thêm thành công!")
+	try:
+		if request.method == "POST":
+			data = request.json
+			user = data['user']
+			title= data['title']
+			deadline = data['deadline']
+			content = data['content']
+			init = str(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+			#a = json.dumps({title:content})
+			if title:
+				conn = mysql.connect()
+				cursor = conn.cursor()
+				cursor.execute("SELECT title FROM job WHERE title='"+title+"'")
+				data = cursor.fetchone()
+				if data is None:
+					sql = "INSERT INTO job VALUES('','"+user+"','"+init+"','"+deadline+"','"+init+"','"+title+"','"+content+"')"
+					cursor.execute(sql)
+					conn.commit()
+					return jsonify(status=0,message="Chúc mừng! Đã thêm thành công!")
+				else:
+					return jsonify(status=1,message="Tiêu đề bị trùng! Đổi lại tiêu đề khác nhé!")
 			else:
-				return jsonify(status=1,message="Tiêu đề bị trùng! Đổi lại tiêu đề khác nhé!")
-		else:
-			return jsonify(status=1,message="Tiêu đề không được bỏ trống!")
-
+				return jsonify(status=1,message="Tiêu đề không được bỏ trống!")
+	except:
+		return jsonify(status=1,message="Lỗi không xác định!")
 
 
 @app.route('/check')
@@ -75,24 +101,27 @@ def login():
 
 @app.route("/login",methods=["POST"])
 def check_login():
-	if request.method == "POST":
-		user = request.form['email']
-		password = request.form['password']
-		if user and password:
-			cursor = mysql.connect().cursor()
-			cursor.execute("SELECT * FROM users WHERE username='%s' AND password='%s'"%(user,password))
-			data = cursor.fetchone()
-			if data is not None:
-				session['user'] = user
-				if str(data[3]):
-					session['is_admin'] = 1
-				else:
-					session['is_admin'] = 0
+	try:
+		if request.method == "POST":
+			user = request.form['email']
+			password = request.form['password']
+			if user and password:
+				cursor = mysql.connect().cursor()
+				cursor.execute("SELECT * FROM users WHERE username='%s' AND password='%s'"%(user,password))
+				data = cursor.fetchone()
+				if data is not None:
+					session['user'] = user
+					if str(data[3]):
+						session['is_admin'] = 1
+					else:
+						session['is_admin'] = 0
 
-				return jsonify(mess="Đăng nhập thành công!",result=1)
-			else:
-				return jsonify(mess="Đăng nhập thất bại!")
-		return	jsonify(mess="Không được bỏ trống!")
+					return jsonify(mess="Đăng nhập thành công!",result=1)
+				else:
+					return jsonify(mess="Đăng nhập thất bại!")
+			return	jsonify(mess="Không được bỏ trống!")
+	except:
+		return jsonify(mess="Lỗi không xác định!")
 
 @app.route('/logout')
 def logout():
